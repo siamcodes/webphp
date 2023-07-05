48e791e9513bd4d0512edb4445886c2e2e7ac66d
siamcodes <siamcodes@gmail.com>
2023-07-05 11:34:10 +0700

    first commit

---
+++ member_insert.php
@@ -0,0 +1,15 @@
+<?php
+include "connect.php";
+
+$sql = "INSERT INTO tbl_member (firstname, lastname, email, phone, photo, address)
+VALUES ('$_POST[firstname]', '$_POST[lastname]', '$_POST[email]', '$_POST[phone]', '$_POST[photo]', '$_POST[address]')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+?>
